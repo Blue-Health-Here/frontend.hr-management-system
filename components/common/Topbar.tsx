44794dc5ea6494a07a30abc7efe1be96139b5df6
Mehreen47 <mehreen@bluehealthhere.com>
2025-05-19 06:37:42 +0500

    update the design of the topbar for better look

--- components/common/Topbar.tsx
+++ components/common/Topbar.tsx
@@ -4,32 +4,32 @@ import { IoSearch } from "react-icons/io5";
 import { IoNotificationsOutline } from "react-icons/io5";
 const Topbar = () => {
     return (
-        <div className="flex justify-between items-center  pl-8 p-4 bg-white fixed top-0 left-64 right-0 z-10">
+        <div className="flex justify-between items-center  pl-8 p-3 bg-white fixed top-0 left-64 right-0 z-10">
             <div className="relative w-[390px] sm:max-w-md">
-                <input type="text" placeholder="Search..." className="w-full md:w-80 bg-gray-100 pl-9 p-2 rounded-lg border border-gray-300" />
-                <span className="absolute left-3 top-2.5 text-gray-600 cursor-pointer">
+                <input type="text" placeholder="Search..." className="w-full md:w-80 bg-gray-50 pl-9 p-2 placeholder:text-gray-400 rounded-lg focus:outline-none neo-down border-none" />
+                <span className="absolute left-3 top-2.5 text-gray-400 cursor-pointer">
                     <IoSearch className="w-4 h-4 md:w-5 md:h-5" />
                 </span>
             </div>
             <div className="flex items-center justify-end gap-4">
                 <div className="flex items-center gap-2">
-                    <div className="relative p-2 rounded-lg border border-gray-300 cursor-pointer">
-                        <IoNotificationsOutline className='h-6 w-6 text-gray-600' />
+                    <div className="relative p-2 rounded-lg neo-down border-none cursor-pointer">
+                        <IoNotificationsOutline className='h-6 w-6 text-gray-400' />
                         <div className="w-4 h-4 rounded-full bg-red-500 text-white absolute text-center text-xs top-1.5 right-1"><p>2</p></div>
                     </div>
-                    <div className="relative p-2 rounded-lg border border-gray-300 cursor-pointer">
-                        <Mail className="w-6 h-6 text-gray-600" />
+                    <div className="relative p-2 rounded-lg neo-down border-none cursor-pointer">
+                        <Mail className="w-6 h-6 text-gray-400" />
                         <div className="w-4 h-4 rounded-full bg-red-500 text-white absolute text-center text-xs top-1.5 right-1"><p>8</p></div>                    </div>
                   </div>
-                <div className="flex items-center gap-2  cursor-pointer">
+                <div className="flex items-center gap-2 shadow-[inset_4px_0_0_#e5e5e5] border-none pl-3 py-1 cursor-pointer">
                     <Image
                         src="/profile-image.jpg" alt="Menu" width={40} height={40} className="rounded-full"
                     />
                     <div className="text-left">
                         <p className="text-sm md:text-base font-bold text-gray-800">John Doe</p>
-                        <p className="text-xs md:text-sm text-gray-500">Admin</p>
+                        <p className="text-xs md:text-sm text-gray-400">Admin</p>
                     </div>
-                    <ChevronDown className="w-4 h-4 text-gray-600" />
+                    <ChevronDown className="w-4 h-4 text-gray-400" />
                 </div>
             </div>
         </div>
