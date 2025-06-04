d3b4be52ede9f3cdf44d3c7caeafa7d163ea5486
mohsinbluehealth <mohsin@bluehealthhere.com>
2025-06-05 00:00:40 +0500

    fixed topbar tweaks responsiveness in mobile devices

--- components/common/Topbar.tsx
+++ components/common/Topbar.tsx
@@ -67,38 +67,19 @@ const Topbar = () => {
           </span>
         </div>
         <div className="flex items-center justify-end gap-6">
-          <div className="flex items-center gap-2">
+          <div className="items-center gap-2 hidden lg:flex">
             <div className="relative p-2 rounded-lg neo-down border-none cursor-pointer">
               <IoNotificationsOutline className='h-6 w-6 text-gray-400' />
               <div className="w-4 h-4 rounded-full bg-red-500 text-white absolute text-center text-xs top-1.5 right-1"><p>2</p></div>
             </div>
             <div className="relative p-2 rounded-lg neo-down border-none cursor-pointer">
               <Mail className="w-6 h-6 text-gray-400" />
-              <div className="w-4 h-4 rounded-full bg-red-500 text-white absolute text-center text-xs top-1.5 right-1"><p>8</p></div>                    </div>
-          </div>
-          <div className="flex items-center gap-2 relative cursor-pointer">
-            <span className="absolute left-0 top-[50%] border-l-2 border-gray-200 translate-x-[-50%] translate-y-[-50%]"></span>
-            <Image src="/profile-image.jpg" alt="Menu" width={40} height={40} className="rounded-full" />
-            <div className="text-left">
-              <p className="text-sm md:text-base font-bold text-gray-800">John Doe</p>
-              <p className="text-xs md:text-sm text-gray-400">Admin</p>
+              <div className="w-4 h-4 rounded-full bg-red-500 text-white absolute text-center text-xs top-1.5 right-1"><p>8</p></div>
             </div>
-            <ChevronDown className="w-4 h-4 text-gray-400" />
           </div>
-          <div
-            className="flex items-center gap-2 pl-6 py-1 relative cursor-pointer"
-            ref={dropdownRef}
-            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-          >
-            <span className="absolute left-0 top-[50%] h-[26px] border-l-2 border-gray-200 translate-x-[-50%] translate-y-[-50%]"></span>
-            <Image
-              src="/profile-image.jpg"
-              alt="Profile"
-              width={40}
-              height={40}
-              className="rounded-full"
-              priority
-            />
+          <div className="flex items-center gap-2 pl-6 relative cursor-pointer" ref={dropdownRef} onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+            <span className="absolute left-0 top-[50%] w-1 h-6 border-l-2 hidden lg:inline border-gray-200 translate-x-[-50%] translate-y-[-50%]"></span>
+            <Image src="/profile-image.jpg" alt="Menu" width={40} height={40} className="rounded-full" />
             <div className="text-left">
               <p className="text-sm md:text-base font-bold text-gray-800">John Doe</p>
               <p className="text-xs md:text-sm text-gray-400">Admin</p>
