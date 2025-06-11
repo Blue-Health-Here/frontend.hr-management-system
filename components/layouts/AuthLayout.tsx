1dd9f7b6cabe18fe404d1a5c750280a1dadc12c1
Mehreen47 <mehreen@bluehealthhere.com>
2025-06-12 01:45:55 +0500

    update the sign in page design according to the new figma design

--- components/layouts/AuthLayout.tsx
+++ components/layouts/AuthLayout.tsx
@@ -1,16 +1,18 @@
 import React, { ReactNode } from "react";
-import AuthLayoutContent from "../auth/AuthLayoutContent";
 
 interface AuthLayoutProps {
-  children: ReactNode; 
+  children: ReactNode;
+  leftContent: React.ReactNode;
 }
 
-function AuthLayout({ children }: AuthLayoutProps) {
+function AuthLayout({ leftContent, children }: AuthLayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      <AuthLayoutContent />
-      <div className="w-full md:w-1/2 flex flex-col justify-between gap-6 py-10 px-6 md:px-8 bg-primary-white">
+      <div className="min-h-screen flex justify-center items-center px-18">
+        <div className="flex flex-col md:flex-row p-10 w-full bg-secondary-bg-theme rounded-4xl">
+        {leftContent}
+      <div className="w-full h-full md:w-1/2 flex flex-col justify-between gap-6 py-10 px-6 md:px-8">
         {children}
+      </div>
       </div>
     </div>
   );
