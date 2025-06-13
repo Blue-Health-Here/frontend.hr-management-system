55849efd7c7cd198f447b1743aed384efdb1617a
Mehreen47 <mehreen@bluehealthhere.com>
2025-06-13 22:26:45 +0500

    design the reset password screen

---
+++ app/(auth-pages)/reset-password/page.tsx
@@ -0,0 +1,24 @@
+import AuthLayout from "@/components/layouts/AuthLayout";
+import ForgotPasswordForm from "@/components/auth/ForgotPasswordForm";
+import ResetPassowrd from "@/components/auth/ResetPassowrd";
+
+export const generateMetadata = () => ({
+  title: "Forgot Password | SmartHR",
+  description:
+    "Reset your SmartHR account password. Enter your email address to receive password reset instructions",
+  keywords: [
+    "forgot password",
+    "password reset",
+    "SmartHR login",
+    "HR management",
+    "account recovery",
+  ],
+});
+
+export default function ForgotPasswordPage() {
+  return (
+    <AuthLayout>
+      <ResetPassowrd />
+    </AuthLayout>
+  );
+}
---
+++ components/auth/ResetPassowrd.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { forgotPasswordInitialVals } from "@/utils/initialVals";
+import { ForgotPasswordValidationSchema } from "@/utils/validationSchema";
+import { Form, Formik } from "formik";
+import InputField from "../common/form/InputField";
+import Button from "../common/Button";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+
+
+export default function ResetPassowrd() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleSubmit = (values: any) => {
+        console.log("Form submitted:", values);
+    };
+
+    return (
+        <>
+            <h1 className="text-green-600 text-3xl font-bold">SmartHR</h1>
+            <div className="flex flex-col items-center w-full max-w-md mx-auto flex-1 justify-center">
+                <div className="space-y-14 w-full">
+                    <div className="mb-8 text-center flex flex-col items-center">
+                        <div className="relative w-32 h-32 flex items-center justify-center mb-4">
+                            <div className="absolute w-20 h-12 bg-green-700 transform -rotate-45 translate-x-2 translate-y-4 z-0" />
+                            <div className="absolute w-20 h-12 bg-green-500 transform -rotate-45 z-10" />
+                        </div>
+                        <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold text-primary-black mb-1">
+                            Set a new password
+
+                        </h1>
+                        <p className="text-secondary-black text-sm md:text-base">
+                            Create a new password. Ensure it differs from previous ones for security
+
+                        </p>
+                    </div>
+                    <Formik
+                        initialValues={forgotPasswordInitialVals}
+                        validationSchema={ForgotPasswordValidationSchema}
+                        onSubmit={handleSubmit}
+                    >
+                        <Form className="space-y-4 w-full">
+                            <InputField
+                                name="password"
+                                type={showPassword ? "text" : "password"}
+                                label="Password"
+                                icon={
+                                    <button
+                                        type="button"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        className="focus:outline-none"
+                                    >
+                                        {showPassword ? (
+                                            <EyeOff className="h-4 w-4 text-quaternary-dark-gray" />
+                                        ) : (
+                                            <Eye className="h-4 w-4 text-quaternary-dark-gray" />
+                                        )}
+                                    </button>
+                                }
+                            />
+                            <InputField
+                                name="confirmPassword"
+                                type={showPassword ? "text" : "password"}
+                                label="Confirm Password"
+                                icon={
+                                    <button
+                                        type="button"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        className="focus:outline-none"
+                                    >
+                                        {showPassword ? (
+                                            <EyeOff className="h-4 w-4 text-quaternary-dark-gray" />
+                                        ) : (
+                                            <Eye className="h-4 w-4 text-quaternary-dark-gray" />
+                                        )}
+                                    </button>
+                                }
+                            />
+
+                            <Button type="submit" label="Update Password" className="mt-2" />
+                        </Form>
+                    </Formik>
+                </div>
+            </div>
+        </>
+    )
+}
